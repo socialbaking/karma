528db10b19d823370354f63e56119e7369a015b1
Mike Mackenzie <mike@veb.co.nz>
2022-08-06 16:03:01 +1200

    Create basic product header

---
+++ components/product/header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold">
+        <span className="text-green-600">
+          CannaSPY
+        </span>
+        {' '} Products
+      </h1>
+    </div>
+  )
+}
+
+export default Header
