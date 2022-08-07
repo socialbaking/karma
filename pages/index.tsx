48b0f94c9419273c8b2b24b580040690dbbbf6df
Mike Mackenzie <mike@veb.co.nz>
2022-08-07 15:07:19 +1200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+import Features from '@/components/features'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="h-screen flex flex-col">
       <Header />
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <span className="text-green-600">
-            CannaSPY
-          </span>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Your experiences. Your data. Your insights.
-        </p>
+      <main className="flex w-full flex-1 flex-col items-center px-20">
+        <Features />
       </main>
-
       <Footer />
     </div>
   )
