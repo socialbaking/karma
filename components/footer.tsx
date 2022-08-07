7784dcae93034d3b80b3fecef4aecdffaed972e2
Mike Mackenzie <mike@veb.co.nz>
2022-08-07 15:07:02 +1200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import logo from "../public/logo.png"
 
 function Footer() {
   return (
-    <footer className="text-gray-600 body-font">
+    <footer className="bg-gray-800" aria-labelledby="footer-heading">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <p className="flex title-font font-medium items-center md:justify-start justify-center text-green-600">
           <Image className="justify-self-center" src={logo} width={50} height={50} alt="CannaSPY Logo" />
