facefb18ba4a3f8d8ef7b9d93bf0469adf87032f
Mike Mackenzie <mike@veb.co.nz>
2022-08-07 15:08:35 +1200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -4,8 +4,8 @@ import logo from "@/public/logo.png"
 
 const navigation = [
   { name: 'Home', href: '/' },
-  { name: 'About', href: '/about' },
-  { name: 'Contact', href: '/contact' },
+  { name: 'Products', href: '/products' },
+  // { name: 'Contact', href: '/contact' },
 ]
 
 function Header() {
@@ -50,33 +50,6 @@ function Header() {
           </div>
         </nav>
       </header>
-    </div>
-  )
-  return (
-    <div>
-      <Head>
-        <title>CannaSPY</title>
-        <meta property="og:title" content="CannaSPY" key="title" />
-        <link rel="icon" href="/logo.png" />
-      </Head>
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <h1 className="flex title-font font-medium items-center md:justify-start justify-center text-green-600">
-          <Image className="justify-self-center" src={logo} width={50} height={50} alt="CannaSPY Logo" />
-          <span className="ml-3 text-4xl">CannaSPY</span>
-        </h1>
-      </div>
-      {/* <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <span className="text-green-600">
-            CannaSPY
-          </span>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Your experiences. Your data. Your insights.
-        </p>
-      </div> */}
     </div>
   )
 }
