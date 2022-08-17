b6588904b33f7cbccd7ca0fef424bb3aa5f86e35
Mike Mackenzie <mike@veb.co.nz>
2022-08-17 16:48:06 +1200

    Create Logo.tsx

---
+++ components/logo/Logo.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Image from 'next/image'
+import DefaultLogo from "../../public/SVG/default.svg";
+
+const Logo = ({
+  primary = "",
+  secondary = "",
+  size = "1rem",
+  fontProps = {},
+  iconOnly = false
+}) => {
+  const logoStyle = {
+    width: `calc(${size} + 20px)`,
+    height: `calc(${size} + 20px)`
+  };
+  const primaryStyle = {
+    fontWeight: "normal",
+    fontFamily: "PT Sans",
+    fontSize: size,
+    paddingLeft: "10px",
+    ...fontProps
+  };
+  const secondaryStyle = {
+    fontWeight: "bold",
+    fontFamily: "PT Sans",
+    fontSize: size,
+    ...fontProps
+  };
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "row",
+    alignContent: "center",
+    alignItems: "flex-end"
+  };
+  return (
+    <div style={containerStyle}>
+      {/* <img
+        alt={`${primary}-${secondary}`}
+        src={DefaultLogo}
+        style={logoStyle}
+      /> */}
+            {/* <Image className={logoStyle} src={DefaultLogo} width={50} height={50} alt="CannaSPY Logo" /> */}
+      <Image className="justify-self-center" src={DefaultLogo} width={50} height={50} alt="CannaSPY Logo" />
+      {!iconOnly && (
+        <>
+          <span style={primaryStyle}>{primary}</span>
+          <span style={secondaryStyle}>{secondary}</span>
+        </>
+      )}
+    </div>
+  );
+};
+
+export { Logo };
