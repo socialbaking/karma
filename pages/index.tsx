08caf250e0c061b4ea0384c5ad7f64b2cb552234
Mike Mackenzie <mike@veb.co.nz>
2022-08-18 10:15:52 +1200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 import Features from '@/components/features'
