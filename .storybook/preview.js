a56184555f2fd9534e200621b32afae7892e850b
Mike Mackenzie <mike@veb.co.nz>
2023-02-13 13:20:06 +1300

    Added Storybook and MSW

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import { addDecorator } from '@storybook/react';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+initialize();
+
+export const decorators = [mswDecorator];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
