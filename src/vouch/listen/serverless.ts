9c676ae8b9c66f87ff6ad232d16127dd2d89b8c9
Fabian Cook <hello@fabiancook.dev>
2023-04-29 21:29:57 +1200

    handler

--- src/vouch/listen/serverless.ts
+++ src/vouch/listen/serverless.ts
@@ -5,7 +5,7 @@ import { create } from "./start";
 
 const app = await create();
 
-export default async (request: unknown, response: unknown) => {
+export async function handler(request: unknown, response: unknown) {
     await app.ready();
     app.server.emit('request', request, response);
 }
