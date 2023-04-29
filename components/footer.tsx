b9269dcd2be8946d088502b4d1b6e6dbbb417de6
Mike Mackenzie <mike@veb.co.nz>
2023-04-29 16:13:26 +1200

    memo'd the footer, stop the re-renders.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,24 +1,22 @@
-// import Image from 'next/image'
-// import logo from "../public/logo.png"
-import Logo from '@/components/logo'
+import React from "react";
+import Logo from "@/components/logo";
+
+const FooterContent = () => (
+	<div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+		<div className="flex title-font font-medium items-center md:justify-start justify-center text-green-600">
+			<Logo primary="pharma" secondary="karma" />
+		</div>
+	</div>
+);
+
+const MemoizedFooterContent = React.memo(FooterContent);
 
 function Footer() {
-  return (
-    <footer className="bg-gray-800" aria-labelledby="footer-heading">
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <p className="flex title-font font-medium items-center md:justify-start justify-center text-green-600">
-          <Logo primary="pharma" secondary="karma" />
-        </p>
-        {/* <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a className="ml-3 text-gray-500">
-            <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-            </svg>
-          </a>
-        </span> */}
-      </div>
-    </footer>
-  )
+	return (
+		<footer className="bg-gray-800" aria-labelledby="footer-heading">
+			<MemoizedFooterContent />
+		</footer>
+	);
 }
 
-export default Footer
+export default Footer;
