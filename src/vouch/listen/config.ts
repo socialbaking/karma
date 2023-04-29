5f730dbb8731c66e9ca7aec16bfca1bbacd0fb95
Fabian Cook <hello@fabiancook.dev>
2023-04-29 23:08:47 +1200

    https://VERCEL_URL

--- src/vouch/listen/config.ts
+++ src/vouch/listen/config.ts
@@ -18,7 +18,7 @@ export function getPort() {
 
 function getEnvironmentHostname() {
     if (process.env.VERCEL_URL) {
-        return process.env.VERCEL_URL
+        return `https://${process.env.VERCEL_URL}`
     }
 
     if (process.env.API_URL) {
