597a49eed1579639ed92cf6ca291aeb231b76de9
Mike Mackenzie <mike@veb.co.nz>
2023-04-29 16:14:39 +1200

    Cleaned this file up. Used memo here too.

--- components/product/listItem.tsx
+++ components/product/listItem.tsx
@@ -1,84 +1,68 @@
-/* eslint-disable no-undef */
-import React from "react";
+import React, { useMemo } from "react";
 import ProductType from "types/product.d";
 import { CalendarIcon, GlobeIcon } from "@heroicons/react/solid";
 
+const PercentageLabel = React.memo(({ value, isHigher }: { value: number; isHigher: boolean }) => (
+  <span
+    className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
+      isHigher ? "bg-green-400" : "bg-green-100"
+    } text-green-800`}
+  >
+    {value}%
+  </span>
+));
+
 function ListItem({ id, attributes }: ProductType) {
-	// const INGREDIENTS = ["CBD", "THC", "CBG"];
-	// console.log('ingredients', INGREDIENTS);
-	// console.log('attributes', attributes);
-	// const ingredients = useMemo(() => {
-	//   if (!attributes) return [];
-	//   return [...INGREDIENTS].filter((key) => attributes[key]).sort((a, b) => attributes[a] < attributes[b] ? -1 : 1)
-	// }, [attributes]);
-	// console.log("ingredients xyz", ingredients);
+  const productUrl = `/products/${id}`;
 
-	const productUrl = `/products/${id}`;
+  const isTHCHigher = useMemo(() => attributes?.THC > attributes?.CBD, [attributes]);
+  const formattedDate = useMemo(() => {
+    if (!attributes?.createdAt) return "";
 
-	return (
-		<li key={id}>
-			<a href={productUrl} className="block hover:bg-gray-50">
-				<div className="px-4 py-4 sm:px-6">
-					<div className="flex items-center justify-between">
-						<p className="text-sm font-medium text-indigo-600 truncate">
-							{attributes?.name}
-						</p>
-						<div className="ml-2 flex-shrink-0 flex">
-							{/* {ingredients.map(key => (
-              <p key={key} className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${attributes?.THC > attributes?.CBD ? "bg-green-400" : "bg-green-100"} bg-green-100 text-green-800`}>
-                {key}: {attributes?.[key]}%
+    return new Intl.DateTimeFormat("en-NZ", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    }).format(new Date(attributes.createdAt));
+  }, [attributes?.createdAt]);
+
+  return (
+    <li key={id}>
+      <a href={productUrl} className="block hover:bg-gray-50">
+        <div className="px-4 py-4 sm:px-6">
+          <div className="flex items-center justify-between">
+            <p className="text-sm font-medium text-indigo-600 truncate">{attributes?.name}</p>
+            <div className="ml-2 flex-shrink-0 flex">
+              <PercentageLabel value={attributes?.THC ?? 0} isHigher={isTHCHigher} />
+              <PercentageLabel value={attributes?.CBD ?? 0} isHigher={!isTHCHigher} />
+            </div>
+          </div>
+          <div className="mt-2 sm:flex sm:justify-between">
+            <div className="sm:flex">
+              <p className="flex items-center text-sm text-gray-500">
+                <GlobeIcon
+                  className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                {attributes?.category?.data?.attributes?.name}
               </p>
-              ))} */}
-							<p
-								className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-									attributes?.THC > attributes?.CBD
-										? "bg-green-400"
-										: "bg-green-100"
-								} text-green-800`}
-							>
-								THC: {attributes?.THC}%
-							</p>
-							<p
-								className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-									attributes?.THC < attributes?.CBD
-										? "bg-green-400"
-										: "bg-green-100"
-								} text-green-800`}
-							>
-								CBD: {attributes?.CBD}%
-							</p>
-						</div>
-					</div>
-					<div className="mt-2 sm:flex sm:justify-between">
-						<div className="sm:flex">
-							<p className="flex items-center text-sm text-gray-500">
-								<GlobeIcon
-									className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-									aria-hidden="true"
-								/>
-								{attributes?.category?.data?.attributes?.name}
-							</p>
-						</div>
-						<div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-							<CalendarIcon
-								className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-								aria-hidden="true"
-							/>
-							<p>
-								Added on{" "}
-								<time dateTime={attributes?.createdAt}>
-									{new Intl.DateTimeFormat("en-NZ", {
-										year: "numeric",
-										month: "2-digit",
-										day: "2-digit",
-									}).format(new Date(attributes?.createdAt))}
-								</time>
-							</p>
-						</div>
-					</div>
-				</div>
-			</a>
+            </div>
+            <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+              <CalendarIcon
+                className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+              {formattedDate && (
+                <p>
+                  Added on <time dateTime={attributes?.createdAt}>{formattedDate}</time>
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      </a>
     </li>
-	);
+  );
 }
-export default ListItem;
+
+export default React.memo(ListItem);
