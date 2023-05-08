50e2cda45b89caad4e7cf058362e119a7f0d3def
Fabian Cook <hello@fabiancook.dev>
2023-05-08 16:59:20 +1200

    Only start tracing if there is HONEYCOMB_API_KEY && OTEL_SERVICE_NAME

--- src/tracing.ts
+++ src/tracing.ts
@@ -14,43 +14,48 @@ const FILTER_HTTP_WORDS = [
 ].flatMap(value => [value, value.toLowerCase(), value.toUpperCase()]);
 const FILTERED_WORD_VALUE = "*****";
 
-// uses the HONEYCOMB_API_KEY and OTEL_SERVICE_NAME environment variables
-export const sdk = new HoneycombSDK({
-    instrumentations: [
-        getNodeAutoInstrumentations({
-            // we recommend disabling fs autoinstrumentation since it can be noisy
-            // and expensive during startup
-            "@opentelemetry/instrumentation-fs": {
-                enabled: false,
-            },
-            "@opentelemetry/instrumentation-fastify": {
-                // TODO Doesn't work
-                requestHook(span, info) {
-                    const { request } = info;
-                    const { query } = request;
-                    const url = new URL(request.url, getOrigin());
-                    // Search can only be completely reset using the string
-                    url.search = new URLSearchParams(
-                        Object.entries(query)
-                            .map(mapEntry)
-                    ).toString();
-                    const { origin } = url;
-                    const urlStringWithoutOrigin = url.toString().replace(origin, "");
-                    span.setAttribute("request.target", urlStringWithoutOrigin);
-                    span.setAttribute("request.url", url.toString());
+const { HONEYCOMB_API_KEY, OTEL_SERVICE_NAME } = process.env;
 
-                    function mapEntry([key, value]: [string, string]) {
-                        if (FILTER_HTTP_WORDS.includes(key)) return [key, FILTERED_WORD_VALUE];
-                        return [key, value]
+export let sdk: HoneycombSDK | undefined = undefined;
+
+if (HONEYCOMB_API_KEY && OTEL_SERVICE_NAME) {
+    sdk = new HoneycombSDK({
+        instrumentations: [
+            getNodeAutoInstrumentations({
+                // we recommend disabling fs autoinstrumentation since it can be noisy
+                // and expensive during startup
+                "@opentelemetry/instrumentation-fs": {
+                    enabled: false,
+                },
+                "@opentelemetry/instrumentation-fastify": {
+                    // TODO Doesn't work
+                    requestHook(span, info) {
+                        const { request } = info;
+                        const { query } = request;
+                        const url = new URL(request.url, getOrigin());
+                        // Search can only be completely reset using the string
+                        url.search = new URLSearchParams(
+                            Object.entries(query)
+                                .map(mapEntry)
+                        ).toString();
+                        const { origin } = url;
+                        const urlStringWithoutOrigin = url.toString().replace(origin, "");
+                        span.setAttribute("request.target", urlStringWithoutOrigin);
+                        span.setAttribute("request.url", url.toString());
+
+                        function mapEntry([key, value]: [string, string]) {
+                            if (FILTER_HTTP_WORDS.includes(key)) return [key, FILTERED_WORD_VALUE];
+                            return [key, value]
+                        }
                     }
                 }
-            }
-        })
-    ],
-});
+            })
+        ],
+    });
 
-sdk.start();
+    sdk.start();
+}
 
 export async function shutdown() {
-    return sdk.shutdown()
+    return sdk?.shutdown()
 }
