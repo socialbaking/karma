c75dff1d8e27142688cc704f4d01b5d5fc24e706
Fabian Cook <hello@fabiancook.dev>
2023-05-09 14:52:30 +1200

    correct store name

--- src/karma/data/product/store.ts
+++ src/karma/data/product/store.ts
@@ -1,7 +1,7 @@
 import {getKeyValueStore} from "../kv";
 import {Product} from "./types";
 
-const STORE_NAME = "report" as const;
+const STORE_NAME = "product" as const;
 
 export function getProductStore() {
     return getKeyValueStore<Product>(STORE_NAME);
