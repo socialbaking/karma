e56e952cf135dcecfeae9934cd533e855ab19430
Fabian Cook <hello@fabiancook.dev>
2023-05-15 12:47:36 +1200

    Metric values

--- src/karma/react/server/paths/metrics.tsx
+++ src/karma/react/server/paths/metrics.tsx
@@ -1,15 +1,43 @@
-import {useMetrics} from "../data/provider";
+import {useMetrics, useProducts} from "../data/provider";
+import {MetricsGraph} from "../../client/components/graph";
 
 export function Metrics() {
     const metrics = useMetrics();
+    const products = useProducts();
     return (
         <div className="metric-list">
             {metrics.map((metric, index) => (
                 <div key={index} className="metric-list-item">
                     <div className="metric-list-item-name">{metric.duration} {metric[metric.reportingDateKey]}</div>
-                    <pre className="metric-list-item-values">
-                        {JSON.stringify(metric.products, undefined, "  ")}
-                    </pre>
+                    <br />
+                    {
+                        metric.products
+                            .map(({ productId, activeIngredients }) => {
+                                const product = products.find(product => product.productId === productId);
+                                if (!product) return undefined;
+                                return (
+                                    <div>
+                                        <strong>Product: {product.productName}</strong><br/>
+                                        <ul className="list-disc list-outside">
+                                            {
+                                                activeIngredients
+                                                    .sort((a, b) => {
+                                                        if (a.type !== b.type) return a.type > b.type ? -1 : 1;
+                                                        if (a.prefix && !b.prefix) return -1;
+                                                        if (b.prefix && !a.prefix) return 1;
+                                                        return a.value < b.value ? -1 : 1;
+                                                    })
+                                                    .map(({ type, unit, value, prefix }) => {
+                                                        return <li>{prefix || ""}{value} {unit} {type}</li>
+                                                    })
+                                            }
+                                        </ul>
+                                        <br />
+                                    </div>
+                                )
+                            })
+                    }
+                    <MetricsGraph metrics={[metric]} />
                 </div>
             ))}
         </div>
