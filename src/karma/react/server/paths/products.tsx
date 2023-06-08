685af6a325fa5f7188d23aa5892b0940e8085b31
Fabian Cook <hello@fabiancook.dev>
2023-06-08 15:29:07 +1200

    Content information banner, closes #79

--- src/karma/react/server/paths/products.tsx
+++ src/karma/react/server/paths/products.tsx
@@ -19,14 +19,48 @@ function ProductListItem(props: ProductProps) {
   return <ClientProductListItem {...props} category={category} isAnonymous={isAnonymous} />;
 }
 
-export function Products() {
+function CopyrightInfo() {
   const products = useSortedProducts(true);
   const copyright = useCopyrightInformation(products);
+  const { isAnonymous } = useData();
+  if (!copyright.length) return null;
+  return (
+      <ul className="list-none">
+        {copyright.map(({ contentUrl, copyrightUrl, label, svg }, index) => (
+            <li key={index} className="flex flex-row items-center my-4">
+              {svg ? <img alt="" role="presentation" src={svg} /> : undefined}
+              <span className="mx-4">
+                This page includes details derived or about&nbsp;
+                <a
+                    href={contentUrl}
+                    target="_blank"
+                    className="text-blue-600 hover:bg-white underline hover:underline-offset-2"
+                >
+                  {(isAnonymous ? COPYRIGHT_PUBLIC_LABEL[contentUrl] : undefined) || "content"}
+                </a>
+                &nbsp;published by&nbsp;
+                <a
+                    href={copyrightUrl}
+                    target="_blank"
+                    className="text-blue-600 hover:bg-white underline hover:underline-offset-2"
+                >
+                  {label}
+                </a>
+              </span>
+            </li>
+        ))}
+      </ul>
+  );
+}
+
+export function Products() {
+  const products = useSortedProducts(true);
   const metrics = useProductMetrics("month");
   const search = useQuerySearch();
   const { isAnonymous } = useData();
   return (
     <>
+      {isAnonymous ? <CopyrightInfo /> : undefined}
       <div className="bg-white shadow overflow-hidden sm:rounded-md">
         <ul role="list" className="divide-y divide-gray-200">
           {products.map((product, index) => (
@@ -47,33 +81,7 @@ export function Products() {
           <br />
         </div>
       ) : undefined}
-      {copyright.length ? (
-        <ul className="list-none">
-          {copyright.map(({ contentUrl, copyrightUrl, label, svg }, index) => (
-            <li key={index} className="flex flex-row items-center my-4">
-              {svg ? <img role="presentation" src={svg} /> : undefined}
-              <span className="mx-4">
-                This page includes details derived or about&nbsp;
-                <a
-                  href={contentUrl}
-                  target="_blank"
-                  className="text-blue-600 hover:bg-white underline hover:underline-offset-2"
-                >
-                  {(isAnonymous ? COPYRIGHT_PUBLIC_LABEL[contentUrl] : undefined) || "content"}
-                </a>
-                &nbsp;published by&nbsp;
-                <a
-                  href={copyrightUrl}
-                  target="_blank"
-                  className="text-blue-600 hover:bg-white underline hover:underline-offset-2"
-                >
-                  {label}
-                </a>
-              </span>
-            </li>
-          ))}
-        </ul>
-      ) : undefined}
+      <CopyrightInfo />
     </>
   );
 }
