063de3055ceb3d01393168e89cb9023f838fcc6d
Fabian Cook <hello@fabiancook.dev>
2023-06-09 22:39:57 +1200

    Get product image by index, #78

--- src/karma/data/product/get-product-file.ts
+++ src/karma/data/product/get-product-file.ts
@@ -20,7 +20,13 @@ export async function listProductFiles(options?: ListProductFilesOptions): Promi
 
 export async function getProductFiles(productId: string, { accept }: GetProductFileListOptions = {}): Promise<File[]> {
     let files = await listNamedFiles("product", productId);
-    files = files.filter(file => file.synced);
+    files = files
+        .filter(file => file.synced)
+        .sort((a, b) => {
+            if (a.pinned && !b.pinned) return -1;
+            if (b.pinned && !a.pinned) return 1;
+            return a.uploadedAt < b.uploadedAt ? -1 : 1
+        });
     if (accept) {
         // "image" will match "image/jpeg"
         files = files.filter(file => file.contentType?.startsWith(accept))
@@ -31,20 +37,21 @@ export async function getProductFiles(productId: string, { accept }: GetProductF
 export interface GetProductFileOptions extends GetProductFileListOptions {
     fileId?: string;
     accept?: string;
+    index?: number;
 }
 
-export async function getProductFile(productId: string, { fileId, accept }: GetProductFileOptions = {}): Promise<File | undefined> {
+export async function getProductFile(productId: string, { fileId, accept, index }: GetProductFileOptions = {}): Promise<File | undefined> {
     if (fileId) {
         const file = await getNamedFile("product", productId, fileId);
         // Must be synced already to be able to get it
         return getMaybeResolvedFile(file);
     }
-    const files = await getProductFiles(productId, { accept });
+    const files = await getProductFiles(productId, { accept })
     if (!files.length) return undefined;
     const pinned = files.filter(file => file.pinned);
     if (pinned.length === 1) return getResolvedFile(pinned[0]);
-    if (pinned.length) return getResolvedFile(pinned[pickIndex(pinned.length)])
-    return getResolvedFile(files[pickIndex(files.length)]);
+    if (pinned.length) return getResolvedFile(pinned[index ?? pickIndex(pinned.length)])
+    return getResolvedFile(files[index ?? pickIndex(files.length)]);
 
     function pickIndex(length: number) {
         const max = length - 1;
